f35fcf6e8276cc7b8c389cd7f806be9f7d39bc4f
Tony Ingle <tonyingle@Tonys-MacBook-Air.local>
2022-05-18 20:50:48 -0600

    fixing staticfiles root

--- total_weather_backend/urls.py
+++ total_weather_backend/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('', include('api.urls')),
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
